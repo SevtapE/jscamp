50295dcc84e4bae4e58da32ab7ddce97a6f13718
SevtapE <sevtaperbek@outlook.com>
2021-10-22 18:41:38 +0300

    Tekrar

--- src/app.js
+++ src/app.js
@@ -1,17 +1,42 @@
 console.log("Merhaba dünya")
-
+console.log("merhaba")
 
 
 let dolarBugun=9.30
-console.log(dolarDun)
 let dolarDun=9.20
 
-let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+{
+    let dolarDun=9.10
+}
+console.log(dolarDun)
 
+const euroDun=11.2
+// euroDun=11
+console.log(euroDun)
+let konutKredileri=["Konut Kredisi","a konut kredisi","b konut kredisi"]
 console.log(konutKredileri)
 
 console.log("<ul>")
-for(let i=0;i<konutKredileri.length;i++){
+
+for (let i = 0; i < konutKredileri.length; i++) {
     console.log("<li>"+konutKredileri[i]+"</li>")
+
 }
-console.log("<ul>")
+console.log("</ul>")
+
+
+
+
+// let dolarBugun=9.30
+// console.log(dolarDun)
+// let dolarDun=9.20
+
+// let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+// console.log(konutKredileri)
+
+// console.log("<ul>")
+// for(let i=0;i<konutKredileri.length;i++){
+//     console.log("<li>"+konutKredileri[i]+"</li>")
+// }
+// console.log("<ul>")
